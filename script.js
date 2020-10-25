99f1fea22f6dc10042cc70f26932f72d5ec8b1fb
JesusJSerrano <jesus.jimenez.serrano.madrid@gmail.com>
2020-10-25 21:34:47 +0300

    add items

--- script.js
+++ script.js
@@ -95,6 +95,31 @@ updatedOnLoad = true;
 updateSavedColumns();
 }
 
+// Add to column list, Reset textbox
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
+
+
+// Show add item input box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide add item input box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
 // Allows arrays to reflect drag and drop items
 function rebuildArrays() {
 backlogListArray = [];
